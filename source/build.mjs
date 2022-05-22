ab185b04aa0f5957ee564c04f03b96ffb1940ad1
Jacob Gray <jgraycs@gmail.com>
2022-05-22 14:03:57 -0400

    Maybe this fix it?

--- source/build.mjs
+++ source/build.mjs
@@ -1,5 +1,33 @@
 import groups from './data/groups.mjs';
 import container from './template/container.mjs';
 import fs from 'fs';
+import https from 'https';
 
-fs.writeFileSync('./public/index.html', container(groups, 'NOW'));
+https.get({
+    hostname: 'api.github.com',
+    path: '/repos/Jacob-Gray/lyh.dance/commits',
+    headers: {
+        'User-Agent': 'Jacob-Gray'
+    }
+}
+    , res => {
+        let output = '';
+
+        res.on('data', data => {
+            output += data;
+        });
+
+        res.on('end', () => {
+            output = JSON.parse(output);
+
+            const mostRecent = output[0];
+            const updateDate = new Date(
+                mostRecent.commit.author.date
+            );
+
+            fs.writeFileSync('./public/index.html', container(
+                groups,
+                updateDate.toLocaleDateString('en-US', { timeZone: 'America/New_York' })
+            ));
+        });
+    });
