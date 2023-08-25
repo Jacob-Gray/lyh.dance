1b8d1c4ce7e8de488ee3f9342a6da3db04774b5f
Jacob Gray <jgraycs@gmail.com>
2023-08-25 19:14:52 -0400

    Updated times

--- source/data/groups.mjs
+++ source/data/groups.mjs
@@ -5,14 +5,14 @@ export default [
         "description": "East Coast Swing and Lindy Hop dance scene focused around a weekly lesson and social dance. High energy, partnered dance to the big band swing of the 1940s.",
         "links": {
             "Facebook": "https://www.facebook.com/lynchburglindyhop/",
-            "Facebook Group": "https://www.facebook.com/groups/53296467121/",
+            "Facebook Group": "https://www.facebook.com/groups/lyhlindy/",
         },
         "schedule": {
-            "text": "Every <b>Tuesday night</b> at <b>8pm</b>. Lesson from 8pm-9pm, social dance until 10:30pm.",
-            description: "Weekly lindy hop social dance. Lesson from 8pm-9pm, social dance until 10:30pm",
+            "text": "Every <b>Tuesday night</b> at <b>8pm</b>. Lesson from 7pm-8pm, social dance until 9:30pm.",
+            description: "Weekly lindy hop social dance. Lesson from 7pm-8pm, social dance until 9:30pm",
             start: {
                 weekday: 2,
-                hour: 20,
+                hour: 19,
             },
             duration: {
                 hour: 2,
@@ -25,20 +25,20 @@ export default [
             "address": "901 Jefferson St G3, Lynchburg, VA 24504"
         }
     },
-//     {
-//         "id": "hcs",
-//         "name": "Hill City Swing",
-//         "description": "West Coast Swing dance scene with weekly lessons and social dance, and monthly Saturday dances. West Coast Swing is a slotted, partnered dance with roots in Lindy Hop; it is danced to a broad range of music, from classic Jazz to modern Pop.",
-//         "links": {
-//             "fb": "https://www.facebook.com/hillcityswing/",
-//         },
-//         "schedule": {
-//             text: "Every <b>Thursday night</b> at <b>8:30pm</b>. Intermediate lesson 8:30pm-9pm, beginner lesson 9pm-9:30pm, social dance until 10:30pm.",
-//         },
-//         "cost": "$5",
-//         "location": {
-//             "name": "The Vibe Studio of Lynchburg",
-//             "address": "1957 Fort Ave, Lynchburg, VA 24501"
-//         }
-//     }
+    {
+        "id": "hcs",
+        "name": "Hill City Swing",
+        "description": "West Coast Swing dance scene with weekly lessons and social dance, and monthly Saturday dances.",
+        "links": {
+            "fb": "https://www.facebook.com/hillcityswing/",
+        },
+        "schedule": {
+            text: "Every <b>Thursday night</b> at <b>8:30pm</b>. Intermediate lesson 8:30pm-9:15pm, beginner lesson 9:15pm-10pm, social dance until 11pm.",
+        },
+        "cost": "$5",
+        "location": {
+            "name": "Live Move Be Fitness Studio",
+            "address": "22273 Timberlake Rd Lynchburg, VA 24502"
+        }
+    }
 ]
